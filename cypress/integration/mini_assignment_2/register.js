76548cb8f50184ec14d44e517149c74ba10b84a7
niharparmar-deloitte <pyogeshbhai@deloitte.com>
2022-04-12 18:07:00 +0530

    changes-made-in-file

--- cypress/integration/mini_assignment_2/register.js
+++ cypress/integration/mini_assignment_2/register.js
@@ -2,23 +2,36 @@ import { cpSync } from "fs"
 
 describe('Validating the site' , () => {
     it('visiting the url',() => {
-        cy.visit("http://demo.automationtesting.in/Register.html")
+        cy.visit('http://demo.automationtesting.in/Register.html')
         cy.title().should("eq","Register")
     })
 
+    var lang1 = 'Danish'
+    var lang2 = 'Dutch'
     it('Selecting value from dropdown',()=>{
-        cy.get('#msdd').click()
-        cy.get('.ui-autocomplete').contains('Danish').click()
-        cy.get('.ui-autocomplete').contains('Dutch').click()
+          cy.visit('http://demo.automationtesting.in/Register.html');
+          cy.get('#msdd').click()
+          cy.get('.ui-autocomplete').contains(lang1).click()
+          cy.get('.ui-autocomplete').contains(lang2).click()
     })
 
     it('Selecting a skill', ()=> {
-        cy.get('#Skills').contains('Oracle').should('have.value','Oracle')
+        cy.visit('http://demo.automationtesting.in/Register.html');
+        cy.get('#Skills').select('Oracle').should('have.value','Oracle')
     })
 
     it('Selecting a country', ()=> {
-        //cy.get('.select2-search__field').type('Ind')
-        cy.get('#country').contains('Denmark').should('have.value','Denmark')
+        cy.visit('http://demo.automationtesting.in/Register.html');
+        cy.get('.select2-selection').click()
+        cy.get('.select2-search__field').type('Aus') 
+        cy.get('ul[class*="select2-results__option"] > li').each((ele,index) => {
+            const cityText = ele.text()
+            if (cityText.includes('Australia'))
+            {
+                cy.get('ul[class*="select2-results__option"] > li').eq(index).click()
+            }
+        })
+
     })
 
 })
